6e0bd7235f57cb0d1227461fae7e1e564fbb2a9d
Nuruddin Kawsar <66555208+kawsar-95@users.noreply.github.com>
2022-04-16 20:42:26 +0600

    first commit

---
+++ components/SubInfo.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const SubInfo = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
---
+++ components/index.js
@@ -0,0 +1,11 @@
+import FocusedStatusBar from './FocusedStatusBar';
+import HomeHeader from './HomeHeader';
+import NFTCard from './NFTCard';
+import { CircleButton, RectButton } from './Button'
+
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  CircleButton, RectButton
+}
